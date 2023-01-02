5a4d2436222eb5b7fcfe9bbbf12090e689e49df6
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2023-01-01 21:38:28 -0300

    Criando o componente Checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,34 @@
+import { StoryObj, Meta } from '@storybook/react';
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react';
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          <Text size="sm">Accept terms of use</Text> {Story()}
+        </Box>
+      );
+    },
+  ],
+} as Meta<CheckboxProps>;
+
+export const Primary: StoryObj<CheckboxProps> = {};
+
+// export const Disabled: StoryObj<CheckboxProps> = {
+//   args: {
+//     disabled: true,
+//   },
+// };
+
+// export const WithPrefix: StoryObj<CheckboxProps> = {
+//   args: {
+//     prefix: 'github.com/users/',
+//   },
+// };
